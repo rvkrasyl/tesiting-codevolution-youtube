757f01fe6550366012941c876c7c5614f41ba49e
rvkrasyl <rvkrasyl@gmail.com>
2023-07-13 12:21:55 +0300

    Adjust tests with getByLabelText search method

--- src/components/application/Application.test.tsx
+++ src/components/application/Application.test.tsx
@@ -2,14 +2,26 @@ import { render, screen } from "@testing-library/react";
 import { Application } from "./Application";
 
 describe("<Applicatin >", () => {
-  test("renders correctly", () => {
+  test("headers renders correctly", () => {
     render(<Application />);
 
-    const nameElement = screen.getByRole("textbox");
+    const pageHeading = screen.getByRole("heading", { level: 1 });
+    const sectionHeading = screen.getByRole("heading", { level: 2 });
+
+    expect(pageHeading).toBeInTheDocument();
+    expect(sectionHeading).toBeInTheDocument();
+  });
+
+  test("form renders correctly", () => {
+    render(<Application />);
+
+    const bioElement = screen.getByRole("textbox", { name: "Bio" }); // same but get by role
+    const nameElement = screen.getByLabelText("Name", { selector: "input" }); // same but get by label
     const jobLocationElement = screen.getByRole("combobox");
     const termsElement = screen.getByRole("checkbox");
     const submitButtonElement = screen.getByRole("button");
 
+    expect(bioElement).toBeInTheDocument(); 
     expect(nameElement).toBeInTheDocument(); 
     expect(jobLocationElement).toBeInTheDocument();
     expect(termsElement).toBeInTheDocument();
