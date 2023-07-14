ff647851af2f1ee251766fa1069e3bf978202bd8
rvkrasyl <rvkrasyl@gmail.com>
2023-07-14 14:11:59 +0300

    adds test to test fetching error handling. Uses reset handlers option

--- src/components/users/Users.test.tsx
+++ src/components/users/Users.test.tsx
@@ -1,5 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import { Users } from "./Users";
+import { server } from "../../mocks/server";
+import { rest } from "msw";
 
 describe('<Users />', () => { 
   test('renders correctly', () => { 
@@ -16,5 +18,19 @@ describe('<Users />', () => {
     const users = await screen.findAllByRole("listitem");
 
     expect(users).toHaveLength(3);
+   });
+
+   test('renders an error message', async () => { 
+    server.use(
+      rest.get('https://jsonplaceholder.typicode.com/users',
+      (req, res, ctx) => {
+        return res(ctx.status(500));
+      })
+    );
+    render(<Users />);
+
+    const errorText = await screen.findByText(/Error fetching/i);
+
+    expect(errorText).toBeInTheDocument();
    });
 });
